6ae866685624d5e220be2fafcedba7329f961ffb
mookar <mookar@Denics-MacBook-Air.local>
2018-09-13 21:55:00 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if(str.length % 2 != 0){
+    return false;
+  };
+  
+  let brackets = bracketsConfig.map(arr=>arr.join(""));
+
+  let iterations = str.length/2;
+
+  while (iterations>0) {
+    for (let j = 0; j < brackets.length; j++) {
+      str = str.replace(brackets[j], "", "g");
+    }
+    iterations--;
+  };
+
+  return str.length == 0;
+
 }
